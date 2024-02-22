ba22e632d5e371425fe44ea3c16bac1b4d5f5a3a
am277 <am2778209@gmail.com>
2024-02-22 13:22:45 +0200

    Fetch News Books UseCase

---
+++ lib/features/home/domain/use_cases/fetch_news_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:bookly/features/home/domain/entites/book_entite.dart';
+import 'package:bookly/features/home/domain/repo/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>,NoParam>{
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase({required this.homeRepo});
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param])async {
+   return await homeRepo.fetchNewestBooks();
+  }
+
+
+}
